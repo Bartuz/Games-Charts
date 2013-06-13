219c3048be5beb55729c314849c3c2f1bda7337c
Bartuz <iamnotawowplayer@gmail.com>
2013-06-12 21:03:38 -0400

    games_data.csv works. cleaned code in get_games_data. Project ready to be factored into html

--- get_games_data.rb
+++ get_games_data.rb
@@ -7,31 +7,13 @@ games = 200
 
 #Setup nokogoiri to capture page with data
 page = Nokogiri::HTML(open("http://www.vgchartz.com/gamedb/?name=&publisher=&platform=&genre=&minSales=0&results=#{games}"))
+# =>  				=> data  0	  1       2     3     4		  5      6 7   8  9    10
+contact_row_data = %w( position title platform year genre publisher na eu jp row global )
 
-#Capture data into arrray. Each element is table row.
-games = []
-
-contact_row_data = %w( title year platform )
-
-CSV.open("file.csv","wb") do |line|
+CSV.open("games_data.csv","wb") do |line|
 	line << contact_row_data
 	page.css('.chart tr').each do |row|
 		data = row.css('td')
-
-		next if data[1].nil?
-		
-		title = data[1].text
-		year = data[3].text
-		platform = data[2].text
-		
-		line << [title,year,platform]
+		line << [data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]] unless data[1].nil?
 	end
 end
-# page.css('.chart tr td:nth-child(2) a').each do |t|
-	# title << t.text
-# end
-# print games[0]
-# # games.text.strip
-# puts games.size
-#Create new file 
-#File.open("games_data.txt" , "w") do |f|
